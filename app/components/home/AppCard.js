788560d8b5e4c1d23d458483b600ac2ad48104e3
muhammad-irfan-ali <irfan.emifusion@gmail.com>
2021-08-30 18:40:15 +0500

    Custom Spring function is written, swip basic animation is done

--- app/components/home/AppCard.js
+++ app/components/home/AppCard.js
@@ -1,42 +1,53 @@
-import configs from "configs";
 import React from "react";
 import { StyleSheet, View, Dimensions } from "react-native";
 import { PanGestureHandler } from "react-native-gesture-handler";
-import Animated, { add } from "react-native-reanimated";
+import Animated, {
+  add,
+  Extrapolate,
+  interpolateNode,
+} from "react-native-reanimated";
 import {
   mix,
   mixColor,
   usePanGestureHandler,
-  withSpring,
 } from "react-native-redash/lib/module/v1";
 
-let { width, height } = Dimensions.get("window");
-width = width * 0.7;
-height = width * (425 / 294);
+import configs from "configs";
+import { customWithSpring } from "layouts/AppAnimations";
 
-export default function AppCard({ position, style }) {
+const { width: wWidth } = Dimensions.get("window");
+const width = wWidth * 0.7;
+const height = width * (425 / 294);
+
+export default function AppCard({ position, style, image, onSwipe, step }) {
   const { gestureHandler, translation, velocity, state } =
     usePanGestureHandler();
   const backgroundColor = mixColor(position, "#C9E9E7", "#74BCB8");
   const translateYOffset = mix(position, 1, -50);
   const scale = mix(position, 1, 0.9);
-
-  const translateX = withSpring({
+  const translateX = customWithSpring({
     value: translation.x,
     velocity: velocity.x,
     state,
-    snapPoints: [-width, 0, width],
+    snapPoints: [-wWidth, 0, wWidth],
+    onSnap: ([x]) => x !== 0 && onSwipe(),
   });
 
   const translateY = add(
     translateYOffset,
-    withSpring({
+    customWithSpring({
       value: translation.y,
       velocity: velocity.y,
       state,
       snapPoints: [0],
     }),
   );
+
+  const imageScale = interpolateNode(position, {
+    inputRange: [0, step],
+    outputRange: [1.2, 1],
+    extrapolate: Extrapolate.CLAMP,
+  });
 
   return (
     <View style={[styles.container, style]}>
@@ -48,7 +59,18 @@ export default function AppCard({ position, style }) {
             height,
             borderRadius: configs.borderRadius.l,
             transform: [{ translateY }, { translateX }, { scale }],
-          }}></Animated.View>
+            overflow: "hidden",
+          }}>
+          <Animated.Image
+            source={image}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              width: undefined,
+              height: undefined,
+              transform: [{ scale: imageScale }],
+            }}
+          />
+        </Animated.View>
       </PanGestureHandler>
     </View>
   );
--- app/screens/OutfitIdeasScreen.js
+++ app/screens/OutfitIdeasScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import configs from "configs";
 import AppHeader from "components/AppHeader";
 import AppBackground from "components/home/AppBackground";
 import AppCard from "components/home/AppCard";
+import { useTransition } from "react-native-redash/lib/module/v1";
+import { interpolateNode, sub } from "react-native-reanimated";
+
+const cards = [
+  { index: 3, source: require("assets/images/4.png") },
+  { index: 2, source: require("assets/images/3.png") },
+  { index: 1, source: require("assets/images/2.png") },
+  { index: 0, source: require("assets/images/1.png") },
+];
+const step = 1 / (cards.length - 1);
 
 export default function OutfitIdeasScreen({ navigation }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const aIndex = useTransition(currentIndex);
   return (
     <View style={styles.screen}>
       <AppHeader
@@ -16,9 +28,18 @@ export default function OutfitIdeasScreen({ navigation }) {
       />
       <View style={styles.container}>
         <AppBackground />
-        <AppCard position={1} />
-        <AppCard position={0.5} />
-        <AppCard position={0} />
+        {cards.map(
+          ({ index, source }) =>
+            currentIndex < index * step + step && (
+              <AppCard
+                key={index}
+                position={sub(index * step, aIndex)}
+                onSwipe={() => setCurrentIndex(prev => prev + step)}
+                image={source}
+                step={step}
+              />
+            ),
+        )}
       </View>
     </View>
   );
